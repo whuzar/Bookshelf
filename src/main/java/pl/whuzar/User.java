8df4bed121c2e6a95afc29a06faf9acbea4b8c9d
whuzar <wojtekhuzar@gmail.com>
2023-06-07 11:54:28 +0200

    added main table user

---
+++ src/main/java/pl/whuzar/User.java
@@ -0,0 +1,113 @@
+package pl.whuzar;
+
+import jakarta.persistence.*;
+
+@Entity(name = "User")
+@Table(name = "user_bookshelf", uniqueConstraints = {@UniqueConstraint(name = "email_unique", columnNames = "email")})
+public class User {
+
+    @Id
+    @SequenceGenerator(name = "user_sequence", sequenceName = "user_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_sequence")
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false, length = 50, columnDefinition = "VARCHAR")
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 50, columnDefinition = "VARCHAR")
+    private String lastName;
+
+    @Column(name = "age", nullable = false, length = 3)
+    private Integer age;
+
+    @Column(name = "email", nullable = false, length = 100, columnDefinition = "VARCHAR")
+    private String email;
+
+    @Column(name = "phone_number", nullable = false, length = 9)
+    private Integer phoneNumber;
+
+    @Column(name = "password", nullable = false, length = 100, columnDefinition = "VARCHAR")
+    private String password;
+
+    public User(String firstName, String lastName, Integer age, String email, Integer phoneNumber, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+    }
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Integer phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                ", phoneNumber=" + phoneNumber +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
